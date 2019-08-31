1bc6d13736a7f00e59e006fd19532340a1277585
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2019-08-31 15:35:05 -0300

    Update CRUD generator command.

--- src/Command/CrudGeneratorCommand.php
+++ src/Command/CrudGeneratorCommand.php
@@ -9,7 +9,11 @@ use Symfony\Component\Console\Input\InputArgument;
 
 class CrudGeneratorCommand extends Command
 {
-    const COMMAND_VERSION = '0.0.8';
+    const COMMAND_VERSION = '0.0.9';
+
+    private $entity;
+
+    private $entityNameUpper;
 
     public function __construct($app)
     {
@@ -32,57 +36,56 @@ class CrudGeneratorCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('Starting!');
-        $entityName = $input->getArgument('entity');
-        $output->writeln('Generate Endpoints For New Entity: ' . $entityName);
-        $this->generateCrud($entityName);
+        $this->entity = $input->getArgument('entity');
+        $this->entityNameUpper = ucfirst($this->entity);
+        $output->writeln('Generate Endpoints For New Entity: ' . $this->entity);
+        $this->generateCrud();
         $output->writeln('Script Finish ;-)');
     }
 
-    protected function generateCrud($entityName)
+    protected function generateCrud()
     {
-        $entityNameUpper = ucfirst($entityName);
-
         // Get Entity Fields.
         $db = $this->container->get('db');
-        $query = "DESC `$entityName`";
+        $query = "DESC `$this->entity`";
         $statement = $db->prepare($query);
         $statement->execute();
         $fields = $statement->fetchAll();
 
         // Get Insert and Update Functions, using each fields of the entity.
-        $repositoryFunctions = $this->getRepositoryFunctions($fields, $entityName, $entityNameUpper);
+        $repositoryFunctions = $this->getRepositoryFunctions($fields);
         $insertQueryFunction = $repositoryFunctions[0];
         $updateQueryFunction = $repositoryFunctions[1];
 
         // Add and Update Routes.
-        $this->updateRoutes($entityName, $entityNameUpper);
+        $this->updateRoutes();
 
         // Add and Update Repository.
-        $this->updateRepository($entityName, $entityNameUpper);
+        $this->updateRepository();
 
         // Add and Update Services.
-        $this->updateServices($entityName, $entityNameUpper);
+        $this->updateServices();
 
         // Generate Controller Files.
-        $this->generateControllerFiles($entityName, $entityNameUpper);
+        $this->generateControllerFiles();
 
         // Replace and Update Exceptions.
-        $this->updateExceptions($entityName, $entityNameUpper);
+        $this->updateExceptions();
 
         // Replace and Update Services.
-        $this->updateServices2($entityName, $entityNameUpper);
+        $this->updateServices2();
 
         // Replace and Update Repository.
-        $this->updateRepository2($entityName, $entityNameUpper);
+        $this->updateRepository2();
 
         // Replace and Update Repository with Insert and Update Query Functions.
-        $this->updateRepository3($entityNameUpper, $insertQueryFunction, $updateQueryFunction);
+        $this->updateRepository3($insertQueryFunction, $updateQueryFunction);
 
         // Create Integration Tests for new endpoints.
-        $this->generateIntegrationTests($entityName, $entityNameUpper, $repositoryFunctions[2]);
+        $this->generateIntegrationTests($repositoryFunctions[2]);
     }
 
-    private function getRepositoryFunctions($fields, $entityName, $entityNameUpper)
+    private function getRepositoryFunctions($fields)
     {
         // Get Dynamic Params and Fields List.
         $paramList = '';
@@ -94,11 +97,11 @@ class CrudGeneratorCommand extends Command
         foreach ($fields as $field) {
             $paramList.= sprintf("`%s`, ", $field['Field']);
             $paramList2.= sprintf(":%s, ", $field['Field']);
-            $paramList3.= sprintf('$statement->bindParam(\'%s\', $%s->%s);%s', $field['Field'], $entityName, $field['Field'], PHP_EOL);
+            $paramList3.= sprintf('$statement->bindParam(\'%s\', $%s->%s);%s', $field['Field'], $this->entity, $field['Field'], PHP_EOL);
             $paramList3.= sprintf("%'\t1s", '');
             if ($field['Field'] != 'id') {
                 $paramList4.= sprintf("`%s` = :%s, ", $field['Field'], $field['Field']);
-                $paramList5.= sprintf("if (isset(\$data->%s)) { $%s->%s = \$data->%s; }%s", $field['Field'], $entityName, $field['Field'], $field['Field'], PHP_EOL);
+                $paramList5.= sprintf("if (isset(\$data->%s)) { $%s->%s = \$data->%s; }%s", $field['Field'], $this->entity, $field['Field'], $field['Field'], PHP_EOL);
                 $paramList5.= sprintf("%'\t1s", '');
                 if ($field['Null'] == "NO" && strpos($field['Type'], 'varchar') !== false) {
                     $paramList6.= sprintf("'%s' => '%s',%s", $field['Field'], 'aaa', PHP_EOL);
@@ -118,35 +121,35 @@ class CrudGeneratorCommand extends Command
         $fieldList6 = substr_replace($paramList6, '', -3);
 
         // Get Base Query For Insert Function.
-        $insertQueryFunction = '$query = \'INSERT INTO `'.$entityName.'` ('.$fieldList.') VALUES ('.$fieldList2.')\';
+        $insertQueryFunction = '$query = \'INSERT INTO `'.$this->entity.'` ('.$fieldList.') VALUES ('.$fieldList2.')\';
         $statement = $this->getDb()->prepare($query);
         '.$fieldList3.'
         $statement->execute();
 
-        return $this->checkAndGet'.$entityNameUpper.'((int) $this->getDb()->lastInsertId());';
+        return $this->checkAndGet'.$this->entityNameUpper.'((int) $this->getDb()->lastInsertId());';
 
         // Get Base Query For Update Function.
         $updateQueryFunction = ''.$fieldList5.'
 
-        $query = \'UPDATE `'.$entityName.'` SET '.$fieldList4.' WHERE `id` = :id\';
+        $query = \'UPDATE `'.$this->entity.'` SET '.$fieldList4.' WHERE `id` = :id\';
         $statement = $this->getDb()->prepare($query);
         '.$fieldList3.'
         $statement->execute();
 
-        return $this->checkAndGet'.$entityNameUpper.'((int) $'.$entityName.'->id);';
+        return $this->checkAndGet'.$this->entityNameUpper.'((int) $'.$this->entity.'->id);';
 
         return [$insertQueryFunction, $updateQueryFunction, $fieldList6];
     }
 
-    private function updateRoutes($entityName, $entityNameUpper)
+    private function updateRoutes()
     {
         $routes = '
-$app->group("/'.$entityName.'", function () use ($app) {
-    $app->get("", "App\Controller\\'.$entityNameUpper.'\GetAll");
-    $app->get("/[{id}]", "App\Controller\\'.$entityNameUpper.'\GetOne");
-    $app->post("", "App\Controller\\'.$entityNameUpper.'\Create");
-    $app->put("/[{id}]", "App\Controller\\'.$entityNameUpper.'\Update");
-    $app->delete("/[{id}]", "App\Controller\\'.$entityNameUpper.'\Delete");
+$app->group("/'.$this->entity.'", function () use ($app) {
+    $app->get("", "App\Controller\\'.$this->entityNameUpper.'\GetAll");
+    $app->get("/[{id}]", "App\Controller\\'.$this->entityNameUpper.'\GetOne");
+    $app->post("", "App\Controller\\'.$this->entityNameUpper.'\Create");
+    $app->put("/[{id}]", "App\Controller\\'.$this->entityNameUpper.'\Update");
+    $app->delete("/[{id}]", "App\Controller\\'.$this->entityNameUpper.'\Delete");
 });
 ';
         $file = __DIR__ . '/../../../../../src/App/Routes.php';
@@ -155,11 +158,11 @@ $app->group("/'.$entityName.'", function () use ($app) {
         file_put_contents($file, $content);
     }
 
-    private function updateRepository($entityName, $entityNameUpper)
+    private function updateRepository()
     {
         $repository = '
-$container["'.$entityName.'_repository"] = function (ContainerInterface $container): App\Repository\\'.$entityNameUpper.'Repository {
-    return new App\Repository\\'.$entityNameUpper.'Repository($container->get("db"));
+$container["'.$this->entity.'_repository"] = function (ContainerInterface $container): App\Repository\\'.$this->entityNameUpper.'Repository {
+    return new App\Repository\\'.$this->entityNameUpper.'Repository($container->get("db"));
 };
 ';
         $file = __DIR__ . '/../../../../../src/App/Repositories.php';
@@ -168,11 +171,11 @@ $container["'.$entityName.'_repository"] = function (ContainerInterface $contain
         file_put_contents($file, $repositoryContent);
     }
 
-    private function updateServices($entityName, $entityNameUpper)
+    private function updateServices()
     {
         $service = '
-$container["'.$entityName.'_service"] = function (ContainerInterface $container): App\Service\\'.$entityNameUpper.'Service {
-    return new App\Service\\'.$entityNameUpper.'Service($container->get("'.$entityName.'_repository"));
+$container["'.$this->entity.'_service"] = function (ContainerInterface $container): App\Service\\'.$this->entityNameUpper.'Service {
+    return new App\Service\\'.$this->entityNameUpper.'Service($container->get("'.$this->entity.'_repository"));
 };
 ';
         $file = __DIR__ . '/../../../../../src/App/Services.php';
@@ -181,65 +184,65 @@ $container["'.$entityName.'_service"] = function (ContainerInterface $container)
         file_put_contents($file, $serviceContent);
     }
 
-    private function generateControllerFiles($entityName, $entityNameUpper)
+    private function generateControllerFiles()
     {
         // Copy CRUD Template.
         $source = __DIR__ . '/../Command/TemplateBase/Objectbase';
-        $target = __DIR__ . '/../../../../../src/Controller/' . $entityNameUpper;
+        $target = __DIR__ . '/../../../../../src/Controller/' . $this->entityNameUpper;
         shell_exec("cp -r $source $target");
 
         // Replace CRUD Controller Template for New Entity.
         $base = $target . '/Base.php';
-        shell_exec("sed -i .bkp -e 's/Objectbase/$entityNameUpper/g' $base");
-        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $base");
-        shell_exec("sed -i .bkp -e 's/Objectbase/$entityNameUpper/g' $target/Create.php");
-        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $target/Create.php");
-        shell_exec("sed -i .bkp -e 's/Objectbase/$entityNameUpper/g' $target/Delete.php");
-        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $target/Delete.php");
-        shell_exec("sed -i .bkp -e 's/Objectbase/$entityNameUpper/g' $target/GetAll.php");
-        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $target/GetAll.php");
-        shell_exec("sed -i .bkp -e 's/Objectbase/$entityNameUpper/g' $target/GetOne.php");
-        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $target/GetOne.php");
-        shell_exec("sed -i .bkp -e 's/Objectbase/$entityNameUpper/g' $target/Update.php");
-        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $target/Update.php");
+        shell_exec("sed -i .bkp -e 's/Objectbase/$this->entityNameUpper/g' $base");
+        shell_exec("sed -i .bkp -e 's/objectbase/$this->entity/g' $base");
+        shell_exec("sed -i .bkp -e 's/Objectbase/$this->entityNameUpper/g' $target/Create.php");
+        shell_exec("sed -i .bkp -e 's/objectbase/$this->entity/g' $target/Create.php");
+        shell_exec("sed -i .bkp -e 's/Objectbase/$this->entityNameUpper/g' $target/Delete.php");
+        shell_exec("sed -i .bkp -e 's/objectbase/$this->entity/g' $target/Delete.php");
+        shell_exec("sed -i .bkp -e 's/Objectbase/$this->entityNameUpper/g' $target/GetAll.php");
+        shell_exec("sed -i .bkp -e 's/objectbase/$this->entity/g' $target/GetAll.php");
+        shell_exec("sed -i .bkp -e 's/Objectbase/$this->entityNameUpper/g' $target/GetOne.php");
+        shell_exec("sed -i .bkp -e 's/objectbase/$this->entity/g' $target/GetOne.php");
+        shell_exec("sed -i .bkp -e 's/Objectbase/$this->entityNameUpper/g' $target/Update.php");
+        shell_exec("sed -i .bkp -e 's/objectbase/$this->entity/g' $target/Update.php");
 
         // Remove Any Temp Files.
         shell_exec("rm -f $target/*.bkp");
     }
 
-    private function updateExceptions($entityName, $entityNameUpper)
+    private function updateExceptions()
     {
         $source = __DIR__ . '/../Command/TemplateBase/ObjectbaseException.php';
-        $target = __DIR__ . '/../../../../../src/Exception/' . $entityNameUpper . 'Exception.php';
+        $target = __DIR__ . '/../../../../../src/Exception/' . $this->entityNameUpper . 'Exception.php';
         shell_exec("cp $source $target");
-        shell_exec("sed -i .bkp -e 's/Objectbase/$entityNameUpper/g' $target");
-        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $target");
+        shell_exec("sed -i .bkp -e 's/Objectbase/$this->entityNameUpper/g' $target");
+        shell_exec("sed -i .bkp -e 's/objectbase/$this->entity/g' $target");
         shell_exec("rm -f $target.bkp");
     }
 
-    private function updateServices2($entityName, $entityNameUpper)
+    private function updateServices2()
     {
         $source = __DIR__ . '/../Command/TemplateBase/ObjectbaseService.php';
-        $target = __DIR__ . '/../../../../../src/Service/' . $entityNameUpper . 'Service.php';
+        $target = __DIR__ . '/../../../../../src/Service/' . $this->entityNameUpper . 'Service.php';
         shell_exec("cp $source $target");
-        shell_exec("sed -i .bkp -e 's/Objectbase/$entityNameUpper/g' $target");
-        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $target");
+        shell_exec("sed -i .bkp -e 's/Objectbase/$this->entityNameUpper/g' $target");
+        shell_exec("sed -i .bkp -e 's/objectbase/$this->entity/g' $target");
         shell_exec("rm -f $target.bkp");
     }
 
-    private function updateRepository2($entityName, $entityNameUpper)
+    private function updateRepository2()
     {
         $source = __DIR__ . '/../Command/TemplateBase/ObjectbaseRepository.php';
-        $target = __DIR__ . '/../../../../../src/Repository/' . $entityNameUpper . 'Repository.php';
+        $target = __DIR__ . '/../../../../../src/Repository/' . $this->entityNameUpper . 'Repository.php';
         shell_exec("cp $source $target");
-        shell_exec("sed -i .bkp -e 's/Objectbase/$entityNameUpper/g' $target");
-        shell_exec("sed -i .bkp -e 's/objectbase/$entityName/g' $target");
+        shell_exec("sed -i .bkp -e 's/Objectbase/$this->entityNameUpper/g' $target");
+        shell_exec("sed -i .bkp -e 's/objectbase/$this->entity/g' $target");
         shell_exec("rm -f $target.bkp");
     }
 
-    private function updateRepository3($entityNameUpper, $insertQueryFunction, $updateQueryFunction)
+    private function updateRepository3($insertQueryFunction, $updateQueryFunction)
     {
-        $target = __DIR__ . '/../../../../../src/Repository/' . $entityNameUpper . 'Repository.php';
+        $target = __DIR__ . '/../../../../../src/Repository/' . $this->entityNameUpper . 'Repository.php';
 
         $entityRepository = file_get_contents($target);
         $repositoryData = preg_replace("/".'#createFunction'."/", $insertQueryFunction, $entityRepository);
@@ -250,14 +253,14 @@ $container["'.$entityName.'_service"] = function (ContainerInterface $container)
         file_put_contents($target, $repositoryDataUpdate);
     }
 
-    private function generateIntegrationTests($entityName, $entityNameUpper, $postParams)
+    private function generateIntegrationTests($postParams)
     {
         $source = __DIR__ . '/../Command/TemplateBase/ObjectbaseTest.php';
-        $target = __DIR__ . '/../../../../../tests/integration/' . $entityNameUpper . 'Test.php';
+        $target = __DIR__ . '/../../../../../tests/integration/' . $this->entityNameUpper . 'Test.php';
         shell_exec("cp $source $target");
         $entityTests = file_get_contents($target);
-        $testsData1 = preg_replace("/".'Objectbase'."/", $entityNameUpper, $entityTests);
-        $testsData2 = preg_replace("/".'objectbase'."/", $entityName, $testsData1);
+        $testsData1 = preg_replace("/".'Objectbase'."/", $this->entityNameUpper, $entityTests);
+        $testsData2 = preg_replace("/".'objectbase'."/", $this->entity, $testsData1);
         $testsData3 = preg_replace("/".'#postParams'."/", $postParams, $testsData2);
         file_put_contents($target, $testsData3);
     }
