2cd966713edbc337d1d3a960811711b592c3f164
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2019-10-07 23:29:31 -0300

    Update version and create a webhook to update packagist on each commit/push event.

--- src/Command/CrudGeneratorCommand.php
+++ src/Command/CrudGeneratorCommand.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Input\InputArgument;
 
 class CrudGeneratorCommand extends Command
 {
-    const COMMAND_VERSION = '0.3.0';
+    const COMMAND_VERSION = '0.4.0';
 
     public function __construct($app)
     {
